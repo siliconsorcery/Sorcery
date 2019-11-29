daad6c74af59ecb6717bf69c743ee48a6eef4c92
John Cumming <john@siliconsorcery.com>
2019-11-29 09:49:53 -0800

    ## 0.0.17 - Remove `Command` / `Response`

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Helpers for iOS App development for UIKit and SwiftUI.
 
 ## New
 
+## 0.0.18
+
+- Remove `Command` / `Response`
+
 ## 0.0.17
 
 - Expose model public offerings
--- Sources/Sorcery/uikit/Models/CommandResponse/ExecuteProtocol.swift
+++ Sources/Sorcery/uikit/Models/CommandResponse/ExecuteProtocol.swift
@@ -8,32 +8,32 @@
 
 import Foundation
 
-public typealias Execute = (Command) -> (Response?)
-
-public protocol ExecuteProtocol {
-    // Provides a command-responce callback
-    var execute: ((Command) -> (Response?))? { get set }
-    // The UIViewController is responsible to release the reference by
-    // setting execute = nil in viewDidDisappear()
-}
-
-// MARK: - Command
-
-public class Command {
-    @discardableResult
-    public func run(_ any: Any? = nil) -> Response {
-        return NilResponse()
-    }
-}
-
-public class NilCommand: Command {}
-public class ExitCommand: Command {}
-public class NavigateCommand: Command {}
-
-// MARK: - Response
-
-public class Response {}
-public class NilResponse: Response {}
-public class CompletedResponse: Response {}
-public class InCompletedResponse: Response {}
-public class DismissResponse: Response {}
+//public typealias Execute = (Command) -> (Response?)
+//
+//public protocol ExecuteProtocol {
+//    // Provides a command-responce callback
+//    var execute: ((Command) -> (Response?))? { get set }
+//    // The UIViewController is responsible to release the reference by
+//    // setting execute = nil in viewDidDisappear()
+//}
+//
+//// MARK: - Command
+//
+//public class Command {
+//    @discardableResult
+//    public func run(_ any: Any? = nil) -> Response {
+//        return NilResponse()
+//    }
+//}
+//
+//public class NilCommand: Command {}
+//public class ExitCommand: Command {}
+//public class NavigateCommand: Command {}
+//
+//// MARK: - Response
+//
+//public class Response {}
+//public class NilResponse: Response {}
+//public class CompletedResponse: Response {}
+//public class InCompletedResponse: Response {}
+//public class DismissResponse: Response {}
